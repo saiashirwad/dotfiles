e5302c5ec0d368812914ba10cc67e4cb598b6f23
Sai Ashirwad <saiashirwadr@gmail.com>
2024-08-16 09:22:50 +0530

    eh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,26 +5,23 @@ end
 
 set EDITOR $(which nvim)
 
-set --export PATH "$HOME/.gem/bin" $PATH
-set --export PATH "$HOME/.pack/bin" $PATH
-set --export PATH "$HOME/.rbenv/shims" $PATH
 set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH "$HOME/.pub-cache/bin" $PATH
-set --export PATH $BUN_INSTALL/bin $PATH
+set --export PATH "$HOME/.cabal/bin" $PATH
+set --export PATH "$HOME/.cargo/bin" $PATH
+set --export PATH "$HOME/.composer/vendor/bin" $PATH
+set --export PATH "$HOME/.gem/bin" $PATH
+set --export PATH "$HOME/.ghcup/bin" $PATH
 set --export PATH "$HOME/.local/bin" $PATH
+set --export PATH "$HOME/.pack/bin" $PATH
+set --export PATH "$HOME/.pub-cache/bin" $PATH
+set --export PATH "$HOME/.rbenv/shims" $PATH
+set --export PATH "$HOME/Library/Android/sdk/tools/bin" $PATH
+set --export PATH "$HOME/Library/Application Support/fnm" $PATH
+set --export PATH "$HOME/apps/nvim-macos-arm64/bin" $PATH
 set --export PATH "$HOME/code/stax/" $PATH
 set --export PATH "$HOME/flutter/bin" $PATH
 set --export PATH "/opt/homebrew/bin" $PATH
-set --export PATH "$HOME/Library/Android/sdk/tools/bin" $PATH
-set --export PATH "$HOME/.cabal/bin" $PATH
-set --export PATH "$HOME/.composer/vendor/bin" $PATH
-set --export PATH "$HOME/apps/nvim-macos-arm64/bin" $PATH
-set --export PATH "$HOME/.cargo/bin" $PATH
-set --export PATH "$HOME/.ghcup/bin" $PATH
-set --export PATH "$HOME/Library/Application Support/fnm" $PATH
-
-alias nvchad 'NVIM_APPNAME=nvchad nvim'
-alias lazyvim 'NVIM_APPNAME=lazyvim nvim'
+set --export PATH $BUN_INSTALL/bin $PATH
 
 fnm env --use-on-cd | source
 
--- phoenix/.config/phoenix/phoenix.js
+++
@@ -1,3 +0,0 @@
-const handler = new Key("q", ["control", "shift"], () => {
-	Window.focused().setTopLeft({ x: 0, y: 0 });
-});
